cade1d6e1c420a5eb317991e2f24f9b2371353a9
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-04-17 20:31:29 +0700

    Add: dracula theme

--- app/src/main/java/eu/kanade/domain/ui/model/AppTheme.kt
+++ app/src/main/java/eu/kanade/domain/ui/model/AppTheme.kt
@@ -5,11 +5,13 @@ import tachiyomi.i18n.MR
 
 enum class AppTheme(val titleRes: StringResource?) {
     DEFAULT(MR.strings.label_default),
-    MONET(MR.strings.theme_monet),
     CATPPUCCIN(MR.strings.theme_catppuccin),
+    DRACULA(MR.strings.theme_dracula),
     GREEN_APPLE(MR.strings.theme_greenapple),
     LAVENDER(MR.strings.theme_lavender),
     MIDNIGHT_DUSK(MR.strings.theme_midnightdusk),
+    MONET(MR.strings.theme_monet),
+    MONOCHROME(MR.strings.theme_monochrome),
     NORD(MR.strings.theme_nord),
     OBSIDIAN(MR.strings.theme_obsidian),
     STRAWBERRY_DAIQUIRI(MR.strings.theme_strawberrydaiquiri),
@@ -19,7 +21,6 @@ enum class AppTheme(val titleRes: StringResource?) {
     TOKYO_NIGHT(MR.strings.theme_tokyonight),
     YINYANG(MR.strings.theme_yinyang),
     YOTSUBA(MR.strings.theme_yotsuba),
-    MONOCHROME(MR.strings.theme_monochrome),
 
     // Deprecated
     DARK_BLUE(null),
