212d9c7ef4ba326fafa848b31a90d2de45e3c792
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-05-25 20:23:05 +0700

    mihon-foss build

--- app/src/main/java/eu/kanade/domain/ui/model/AppTheme.kt
+++ app/src/main/java/eu/kanade/domain/ui/model/AppTheme.kt
@@ -5,19 +5,22 @@ import tachiyomi.i18n.MR
 
 enum class AppTheme(val titleRes: StringResource?) {
     DEFAULT(MR.strings.label_default),
-    MONET(MR.strings.theme_monet),
     CATPPUCCIN(MR.strings.theme_catppuccin),
+    DRACULA(MR.strings.theme_dracula),
     GREEN_APPLE(MR.strings.theme_greenapple),
     LAVENDER(MR.strings.theme_lavender),
     MIDNIGHT_DUSK(MR.strings.theme_midnightdusk),
+    MONET(MR.strings.theme_monet),
+    MONOCHROME(MR.strings.theme_monochrome),
     NORD(MR.strings.theme_nord),
+    OBSIDIAN(MR.strings.theme_obsidian),
     STRAWBERRY_DAIQUIRI(MR.strings.theme_strawberrydaiquiri),
     TAKO(MR.strings.theme_tako),
     TEALTURQUOISE(MR.strings.theme_tealturquoise),
     TIDAL_WAVE(MR.strings.theme_tidalwave),
+    TOKYO_NIGHT(MR.strings.theme_tokyonight),
     YINYANG(MR.strings.theme_yinyang),
     YOTSUBA(MR.strings.theme_yotsuba),
-    MONOCHROME(MR.strings.theme_monochrome),
 
     // Deprecated
     DARK_BLUE(null),
