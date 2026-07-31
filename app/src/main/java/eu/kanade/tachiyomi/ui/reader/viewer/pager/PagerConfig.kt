f2ea7673575555f1fd8d2a260c96c772f55c7025
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-07-31 21:20:21 +0700

    Add disable doubletap option to paged reader

--- app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/pager/PagerConfig.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/reader/viewer/pager/PagerConfig.kt
@@ -48,6 +48,11 @@ class PagerConfig(
     var landscapeZoom = false
         private set
 
+    var doubleTapZoom = false
+        private set
+
+    var doubleTapZoomChangedListener: ((Boolean) -> Unit)? = null
+
     init {
         readerPreferences.readerTheme
             .register(
@@ -105,6 +110,15 @@ class PagerConfig(
             .register(
                 { dualPageRotateToFitInvert = it },
                 { imagePropertyChangedListener?.invoke() },
+            )
+
+        readerPreferences.enableDoubleTapZoomPager
+            .register(
+                { doubleTapZoom = it },
+                {
+                    doubleTapZoomChangedListener?.invoke(it)
+                    imagePropertyChangedListener?.invoke()
+                },
             )
     }
 
