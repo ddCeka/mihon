5442b8bb1fd20dd8fd5b3f540a449e07a049a40b
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-07-31 21:20:21 +0700

    Add disable doubletap option to paged reader

--- app/src/main/java/eu/kanade/presentation/reader/settings/ReadingModePage.kt
+++ app/src/main/java/eu/kanade/presentation/reader/settings/ReadingModePage.kt
@@ -134,6 +134,11 @@ private fun ColumnScope.PagerViewerSettings(viewModel: ReaderSettingsViewModel) 
             pref = viewModel.preferences.dualPageRotateToFitInvert,
         )
     }
+
+    CheckboxItem(
+        label = stringResource(MR.strings.pref_double_tap_zoom),
+        pref = viewModel.preferences.enableDoubleTapZoomPager,
+    )
 }
 
 @Composable
