cf8c6d671de6fff9eb68b12838bc8412d55f8027
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-08-15 05:59:04 +0700

    Revert add "HiGh QuALiTy" webGPU renderer
    The HiGh QuALiTy is actually shit quality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -242,10 +242,6 @@ dependencies {
     }
     implementation(libs.image.decoder)
 
-    implementation(libs.image.decoder2)
-    implementation(libs.webgpuviewer)
-    implementation(libs.kim)
-
     // UI libraries
     implementation(libs.material)
     implementation(libs.flexibleAdapter)
