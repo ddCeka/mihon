ab35ccc56df10008ec7db0f74af8f81f0117545d
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-08-23 01:31:53 +0700

    Add Github theme

--- app/src/main/java/eu/kanade/presentation/theme/TachiyomiTheme.kt
+++ app/src/main/java/eu/kanade/presentation/theme/TachiyomiTheme.kt
@@ -11,6 +11,7 @@ import eu.kanade.domain.ui.UiPreferences
 import eu.kanade.domain.ui.model.AppTheme
 import eu.kanade.presentation.theme.colorscheme.BaseColorScheme
 import eu.kanade.presentation.theme.colorscheme.CatppuccinColorScheme
+import eu.kanade.presentation.theme.colorscheme.GithubColorScheme
 import eu.kanade.presentation.theme.colorscheme.GothamColorScheme
 import eu.kanade.presentation.theme.colorscheme.GreenAppleColorScheme
 import eu.kanade.presentation.theme.colorscheme.LavenderColorScheme
@@ -92,6 +93,7 @@ private fun getThemeColorScheme(
 private val colorSchemes: Map<AppTheme, BaseColorScheme> = mapOf(
     AppTheme.DEFAULT to TachiyomiColorScheme,
     AppTheme.CATPPUCCIN to CatppuccinColorScheme,
+    AppTheme.GITHUB to GithubColorScheme,
     AppTheme.GOTHAM to GothamColorScheme,
     AppTheme.GREEN_APPLE to GreenAppleColorScheme,
     AppTheme.LAVENDER to LavenderColorScheme,
