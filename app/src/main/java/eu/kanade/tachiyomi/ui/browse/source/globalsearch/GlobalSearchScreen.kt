9b6d9009adab148bc555caa21941c629c827b5d9
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-09-05 19:05:57 +0700

    Refactor Pinned Only toggle for global search

--- app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/globalsearch/GlobalSearchScreen.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/browse/source/globalsearch/GlobalSearchScreen.kt
@@ -59,7 +59,7 @@ class GlobalSearchScreen(
                 onChangeSearchQuery = viewModel::updateSearchQuery,
                 onSearch = { viewModel.search() },
                 getManga = { viewModel.getManga(it) },
-                onChangeSearchFilter = viewModel::setSourceFilter,
+                onTogglePinnedOnly = viewModel::togglePinnedOnly,
                 onToggleResults = viewModel::toggleFilterResults,
                 onClickSource = {
                     navigator.push(BrowseSourceScreen(it.id, state.searchQuery))
