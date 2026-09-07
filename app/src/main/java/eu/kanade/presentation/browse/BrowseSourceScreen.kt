2c2cf96dac92a5e82c2855fa1dfd7ca4ebb09831
ddCeka <57752215+ddCeka@users.noreply.github.com>
2026-09-08 02:14:56 +0700

    Add local epub import ui

--- app/src/main/java/eu/kanade/presentation/browse/BrowseSourceScreen.kt
+++ app/src/main/java/eu/kanade/presentation/browse/BrowseSourceScreen.kt
@@ -21,6 +21,7 @@ import eu.kanade.tachiyomi.source.Source
 import kotlinx.coroutines.flow.StateFlow
 import mihon.icons.materialsymbols.MaterialSymbols
 import mihon.icons.materialsymbols.automirroredrounded.Help
+import mihon.icons.materialsymbols.rounded.Add
 import mihon.icons.materialsymbols.rounded.Public
 import mihon.icons.materialsymbols.rounded.Refresh
 import tachiyomi.core.common.i18n.stringResource
@@ -46,6 +47,7 @@ fun BrowseSourceContent(
     onWebViewClick: () -> Unit,
     onHelpClick: () -> Unit,
     onLocalSourceHelpClick: () -> Unit,
+    onLocalSourceImportClick: (() -> Unit)?,
     onMangaClick: (Manga) -> Unit,
     onMangaLongClick: (Manga) -> Unit,
 ) {
@@ -85,13 +87,24 @@ fun BrowseSourceContent(
                 else -> stringResource(MR.strings.no_results_found)
             },
             actions = if (source is LocalSource) {
-                listOf(
-                    EmptyScreenAction(
-                        stringRes = MR.strings.local_source_help_guide,
-                        icon = MaterialSymbols.AutoMirroredRounded.Help,
-                        onClick = onLocalSourceHelpClick,
-                    ),
-                )
+                buildList {
+                    onLocalSourceImportClick?.let { onImportClick ->
+                        add(
+                            EmptyScreenAction(
+                                stringRes = MR.strings.action_import_local_manga,
+                                icon = MaterialSymbols.Rounded.Add,
+                                onClick = onImportClick,
+                            ),
+                        )
+                    }
+                    add(
+                        EmptyScreenAction(
+                            stringRes = MR.strings.local_source_help_guide,
+                            icon = MaterialSymbols.AutoMirroredRounded.Help,
+                            onClick = onLocalSourceHelpClick,
+                        ),
+                    )
+                }
             } else {
                 listOf(
                     EmptyScreenAction(
